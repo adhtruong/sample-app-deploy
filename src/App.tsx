4a2e13f645b4499a5d0985c69afa7ce709012f7d
Andrew Truong <adh.truong@gmail.com>
2020-06-27 11:36:33 +0100

    Update package, add truchet tilings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import p5 from 'p5';
+
+
+let sketch = ( p: p5 ) => {
+  let tiles = 12;
+  let tileSize = 50;
+
+  p.setup = () => {
+      p.createCanvas(tiles * tileSize, tiles * tileSize);
+      p.noLoop();
+  }
+
+  p.draw = () => {
+      p.background(0);
+      p.noFill();
+      p.strokeWeight(10);
+      p.stroke(255);
+      p.strokeCap(p.SQUARE);
+      for (let i=0; i<tiles; i++) {
+        for (let j=0; j<tiles; j++) {
+          p.push()
+          p.translate((i + 0.5) * tileSize, (j +0.5) * tileSize);
+          p.rotate(Math.floor(p.random() * 4) * p.PI / 2);
+          p.arc(-0.5 * tileSize, -0.5 * tileSize, tileSize, tileSize, 0, p.PI/2);
+          p.arc(0.5 * tileSize, 0.5 * tileSize, tileSize, tileSize, p.PI, p.PI * 3/2);
+          p.pop()
+        }
+      }
+  }
+}
+
+
+class App extends React.Component {
+  myRef: React.RefObject<HTMLDivElement>;
+  myP5: p5 | null;
+
+  constructor(props: any) {
+    super(props);
+    this.myRef = React.createRef();
+    this.myP5 = null;
+  }
+
+  componentDidMount() {
+    if (this.myRef.current === null)
+      return
+    this.myP5 = new p5(sketch, this.myRef.current);
+  }
+
+  render() {
+    return (
+      <div 
+      style={{
+        position: "absolute", 
+        left:'50%', 
+        top: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+      ref={this.myRef}>
+      </div>
+    )
+  }
 }
 
 export default App;
