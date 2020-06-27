b7fcbf50130772102a3fd3644556c6e803e9e402
Andrew Truong <adh.truong@gmail.com>
2020-06-27 10:02:38 +0100

    Updates

---
+++ precache-manifest.aab867a61f221c5ebf5020a7279538a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc2a2c94268ae726005ccb01d23a105",
+    "url": "/sample-app/index.html"
+  },
+  {
+    "revision": "f21e557d813db3523e4c",
+    "url": "/sample-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ae91f31304bfac2cd79",
+    "url": "/sample-app/static/js/2.8b0193bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-app/static/js/2.8b0193bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21e557d813db3523e4c",
+    "url": "/sample-app/static/js/main.fc6db27b.chunk.js"
+  },
+  {
+    "revision": "aaa610276e911245a9a2",
+    "url": "/sample-app/static/js/runtime-main.7f6b212c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1f5dea3d603beb067d52130f538b749.js"
+  "/sample-app/precache-manifest.aab867a61f221c5ebf5020a7279538a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sample-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
